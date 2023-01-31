01e9d842ed9c595ceaeb430ffd61c21d05f0e030
Flovedev <flove.dev@gmail.com>
2023-01-31 16:09:26 +0000

    little mistakes

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -4,11 +4,11 @@ import books from '../Data/fantasy.json'
 export default function AllTheBooks() {
     console.log(books)
     return (
-        <Container fluid className="bg-secondary py-4">
-            <Row xs={1} md={2} lg={4} >
+        <Container fluid className="bg-secondary py-4 pl-5">
+            <Row xs={1} md={2} lg={3} xl={4} >
                 {books.map((book) =>{
                 return (
-                <Col className="mb-2">
+                <Col className="mb-2" key={book.asin}>
                     <Card style={{ width: '18rem'}} className="bg-dark text-white">
                         <Card.Img variant="top" src={book.img} />
                         <Card.Body>
--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -2,7 +2,7 @@ import {Container, Row, Col} from 'react-bootstrap';
 
 export default function MyFooter({name}) {
     return (
-      <Container fluid className='fixed-bottom bg-dark text-white'>
+      <Container fluid className='bg-dark text-white'>
         <Row>
           <Col>2023 - {name}</Col>
           <Col className='bg-light text-dark'></Col>
