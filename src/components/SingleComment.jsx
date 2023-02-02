1a4e35c2e7b7155c21a82b7004afaf17afdda0a8
Flovedev <flove.dev@gmail.com>
2023-02-02 16:07:21 +0000

    updates

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,20 +1,11 @@
-import { ListGroup, Badge } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
 
 export default function SingleComment({ comment, rate, elementId }) {
   return (
-    <ListGroup.Item>
-      <ListGroup.Item
-        as="li"
-        className="d-flex justify-content-between align-items-start bg-dark text-white"
-      >
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">{comment}</div>
-          Rate: {rate}
-        </div>
-        <Badge bg="primary" pill>
-          ID: {elementId}
-        </Badge>
-      </ListGroup.Item>
+    <ListGroup.Item className="bg-info text-white mb-2">
+      <p>Comment:{comment}</p>
+      <p>Rate:{rate}</p>
+      <p>ID:{elementId}</p>
     </ListGroup.Item>
   );
 }
