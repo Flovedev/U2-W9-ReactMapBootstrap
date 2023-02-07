800b6314bc877fbda9bae64407ff3f8829adba40
Flovedev <flove.dev@gmail.com>
2023-02-07 12:32:41 +0000

    refactoring in progress

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,22 +1,20 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 
 const url = "https://striveschool-api.herokuapp.com/api/comments/";
 
-class AddComment extends Component {
-  state = {
-    newComment: {
-      comment: "",
-      rate: 1,
-      elementId: "",
-    },
-  };
+const AddComment = ({ data }) => {
+  const [commentUse, setComment] = useState({
+    comment: "",
+    rate: 1,
+    elementId: "",
+  });
 
-  sendComment = async () => {
+  const sendComment = async () => {
     try {
       let response = await fetch(url, {
         method: "POST",
-        body: JSON.stringify(this.state.newComment),
+        body: JSON.stringify(commentUse),
         headers: {
           Authorization:
             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2RmZjAwOWUzYjkwMzAwMTViMDIxMGYiLCJpYXQiOjE2NzU2MjAzNjIsImV4cCI6MTY3NjgyOTk2Mn0.3_KrhUyky1fJuPd-VhuL9frAuhiXZGvrBXzC3Dz4jHE",
@@ -29,69 +27,66 @@ class AddComment extends Component {
     }
   };
 
-  render() {
-    return (
-      <>
-        <Form
-          onSubmit={(e) => {
-            e.preventDefault();
-            this.sendComment();
-          }}
-        >
-          <Form.Group controlId="add-comments">
-            <p className="m-0 text-white">Comment:</p>
-            <Form.Control
-              type="text"
-              placeholder="Type your comment..."
-              onChange={(e) => {
-                this.setState({
-                  newComment: {
-                    ...this.state.newComment,
-                    comment: e.target.value,
-                  },
-                });
-              }}
-            />
-            <p className="m-0 text-white">Rate:</p>
-            <Form.Control
-              as="select"
-              value={this.state.newComment.rate}
-              onChange={(e) => {
-                this.setState({
-                  newComment: {
-                    ...this.state.newComment,
-                    rate: e.target.value,
-                  },
+  useEffect(() => {
+    sendComment();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendComment();
+        }}
+      >
+        <Form.Group controlId="add-comments">
+          <p className="m-0 text-white">Comment:</p>
+          <Form.Control
+            type="text"
+            placeholder="Type your comment..."
+            onChange={(e) => {
+              setComment({
+                ...commentUse,
+                comment: e.target.value,
+              });
+            }}
+          />
+          <p className="m-0 text-white">Rate:</p>
+          <Form.Control
+            as="select"
+            value={commentUse.rate}
+            onChange={(e) => {
+              setComment({
+                ...commentUse,
+                rate: e.target.value,
+              });
+            }}
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Control>
+          <div className="text-center m-2">
+            <Button
+              variant="success"
+              type="submit"
+              onClick={() => {
+                setComment({
+                  ...commentUse,
+                  elementId: data,
                 });
               }}
             >
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </Form.Control>
-            <div className="text-center m-2">
-              <Button
-                variant="success"
-                type="submit"
-                onClick={() => {
-                  this.setState({
-                    newComment: {
-                      ...this.state.newComment,
-                      elementId: this.props.data,
-                    },
-                  });
-                }}
-              >
-                Post Comment
-              </Button>
-            </div>
-          </Form.Group>
-        </Form>
-      </>
-    );
-  }
-}
+              Post Comment
+            </Button>
+          </div>
+        </Form.Group>
+      </Form>
+    </>
+  );
+};
 
 export default AddComment;
